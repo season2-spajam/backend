5003029a470b30de28ea488337d867368b3c3926
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-10-07 15:16:30 +0900

    serializer 追加

--- onechance/views.py
+++ onechance/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Post
+from .serializeres import PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
