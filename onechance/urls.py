214f8c257df7873350cd0f67907255ae7a0f3998
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-10-07 16:32:21 +0900

    細かいとこ修正 (何修正したか忘れました…)

--- onechance/urls.py
+++ onechance/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet
 
 router = DefaultRouter()
-router.register('posts', PostViewSet)
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
 
