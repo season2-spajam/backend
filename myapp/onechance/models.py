ca6de8b46c2f4dc15cdfb0368ac6ac0d2bb03c01
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-10-07 14:44:28 +0900

    モデル定義

--- myapp/onechance/models.py
+++ myapp/onechance/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    handle_name = models.CharField(max_length=255)
+    img = models.ImageField(upload_to='images/')
+    good_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.handle_name + ": " + str(self.created_at)
