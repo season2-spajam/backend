cd25d1fbe57a50a613fcf1099c4fc27a5be8f157
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-10-07 16:11:23 +0900

    エンドポイントの URL 修正

--- onechance/urls.py
+++ onechance/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet
 
 router = DefaultRouter()
-router.register('post', PostViewSet)
+router.register('posts', PostViewSet)
 
 urlpatterns = [
 
