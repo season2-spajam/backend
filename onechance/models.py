f28bf8592fed0458ab86afdcfdc676a0bd4fb33f
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-10-07 15:54:13 +0900

    models に caption を追加

--- onechance/models.py
+++ onechance/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     handle_name = models.CharField(max_length=255)
     img = models.ImageField(upload_to='images/')
     good_count = models.IntegerField(default=0)
+    caption = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
