6ec6cfa0b3cd810f8a3b5e644b21fa73cde949f0
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-10-07 20:30:01 +0900

    settings を更新 (環境変数を設定)

--- myapp/settings.py
+++ myapp/settings.py
@@ -10,23 +10,34 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
 import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+env_root = environ.Path(__file__) - 2
+env.read_env(env_root(".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(1b#0qo46emn=9dd#ibnrjhn+gsy$xx)0h3j808)+$v77%*8!*"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+NGROK_URL = env("NGROK_URL")
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    NGROK_URL,
+]
+
 
 
 # Application definition
